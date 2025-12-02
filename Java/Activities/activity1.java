ee6f88c9798e376e0e0c04ca8b9144e7135a78e2
Bhavyashreebs21 <Bhavyashree.B.S@ibm.com>
2025-12-02 08:45:36 +0530

    Add files via upload

---
+++ Java/Activities/activity1.java
@@ -0,0 +1,15 @@
+package activities;
+
+public class activity1 {
+	
+	public static void main(String[] args) {
+        car toyota = new car("Black", "Manual", 2024);
+         
+        toyota.displayCharacterstics();
+        toyota.accelerate();
+        toyota.brake();
+    }
+
+}
+
+
